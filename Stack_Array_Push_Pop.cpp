908ca7b7c37b04c3965be6c4ddd7809d9e04df8d
khyatijain3105 <87448397+khyatijain3105@users.noreply.github.com>
2021-10-22 23:34:55 +0530

    Add files via upload

---
+++ Stack_Array_Push_Pop.cpp
@@ -0,0 +1,69 @@
+// Implement stack using array with push and pop operations.
+//Codechef ide
+
+#include <iostream>
+using namespace std;
+#define SIZE 5
+
+int stack[SIZE];                   //declaring stack variable globally so we can access the push and pop functions
+int TOP=-1;                        // initially there are no elements to display
+
+void display()
+{
+    if (TOP==-1)
+    cout<<"\n Stack is empty\n";
+    else
+    {
+        cout<<"\n";
+        for(int i=TOP;i>-1;i--)
+        cout<<"\t"<<stack[i];
+    }
+}
+void push(int val)                   //push should satisfy the condition of stack being full.
+{
+    if (TOP==SIZE-1)
+    cout<<"\n Stack is now full";
+    else
+    {
+        stack[++TOP]=val;
+        cout<<"\n Inserted Element:"<<val;
+    }
+}
+
+int pop()
+{
+    if(TOP==-1)
+    cout<<"\n Stack is Empty, no element to pop";
+    else
+    {
+        int temp=stack[TOP];
+        TOP--;
+        return temp;
+    }
+}
+
+int main()
+{ 
+    int t;
+    display();
+    push(3105);
+    push(7005);
+    push(7421);
+    push(2002);
+    push(1857);
+    
+    t=pop();
+    cout<<"\n Element popped:"<<t;
+    t=pop();
+    cout<<"\n Element popped:"<<t;
+    t=pop();
+    cout<<"\n Element popped:"<<t;
+    t=pop();
+    cout<<"\n Element popped:"<<t;
+    t=pop();
+    cout<<"\n Element popped:"<<t;
+    t=pop();
+
+    display();
+	return 0;
+}
